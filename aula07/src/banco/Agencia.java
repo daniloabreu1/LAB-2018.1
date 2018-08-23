f0366bf166bf4f214fa8641b58ed59f6b1ad5f9c
Danilo Abreu <daniloabreu1@gmail.com>
2018-08-23 16:34:13 -0300

    aula07

---
+++ aula07/src/banco/Agencia.java
@@ -0,0 +1,55 @@
+package banco;
+
+/**
+ *
+ * @author Danilo Abreu
+ */
+public class Agencia {
+    private static int qtd=1000;
+    private String nome,cidade, uf;
+    private final int numero;
+
+    public Agencia(String nome) {
+        this.nome = nome;
+        this.numero=++qtd;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void alterarNome(String novoNome) {
+        this.nome = novoNome;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getUf() {
+        return uf;
+    }
+
+    public void setUf(String uf) {
+        this.uf = uf;
+    }
+
+    public static int getQuantidade() {
+        return qtd-1000;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    @Override
+    public String toString() {
+        return numero+ "\nNome da Agencia: " + nome + "\nCidade: " + cidade + "\nUF: " + uf ;
+    }
+    
+    
+}
